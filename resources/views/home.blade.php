b96d0151d19c361a44d3d65e677e423e6b81f4a5
tieppt07 <tieppt07@gmail.com>
2015-11-16 16:39:50 +0700

    home-page frontend

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,52 @@
 @extends('master')
 
 @section('content')
-<div class="container">
-	<div class="row">
-		<div class="col-md-10 col-md-offset-1">
-			<div class="panel panel-default">
-				<div class="panel-heading">Home</div>
-
-				<div class="panel-body">
-					You are logged in!
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">Home</div>
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div class="text-center">
+                                <div class="profile">
+                                    <img src="{{ $currentUser->avatar }}" alt="" width="100px" height="100px"><br><br>
+                                    <p>{{ $currentUser->followers()->count() }} followers | {{ $currentUser->followees()->count() }} follows</p>
+                                    <p><a href="{{ url('users/'.$currentUser->id) }}">{{ $currentUser->name }}</a></p>
+                                    <p>learned 120 words</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-9">
+                            <p class="text-center">
+                                <a href="{{ url('words') }}" class="btn btn-primary btn-lg">Words</a>
+                                <a href="{{ url('lessons') }}" class="btn btn-default btn-lg">Lesson</a>
+                            </p>
+                            <div class="well">
+                                <div class="row">
+                                    <div class="col-sm-12">
+                                        @foreach ($activities as $activity)
+                                            <div class="row">
+                                                <div class="col-sm-12 col-xs-12">
+                                                    <p>
+                                                        <a href="{{ url('users/'.$activity->user->id) }}">{{ $activity->user->name }}</a>
+                                                        starts Lesson <strong>#{{ $activity->id }}</strong>
+                                                        in Category <a href="{{ url('categories/'.$activity->category->id) }}">{{ $activity->category->name }}</a>
+                                                        at <u>{{ $activity->created_at }}</u>
+                                                    </p>  
+                                                </div>
+                                            </div>
+                                        @endforeach
+                                    </div>
+                                    <div class="text-center">
+                                        {!! $activities->render() !!}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div> {{-- /end panel-body --}}
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,59 +1,66 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<title>Framgia E-Learning System - @yield('title')</title>
-	<link rel="shortcut icon" href="{{ asset('favicon(copy).png') }}"/>    
-	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
-	<script src="{{ asset('js/jquery-1.11.3.js') }}"></script>
-	<script src="{{ asset('js/bootstrap.min.js') }}"></script>
-
-	<script src="{{ asset('js/ckeditor/ckeditor.js') }}"></script>
-
-	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-	<!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
+    <title>Framgia E-Learning System - @yield('title')</title>
+    <link rel="shortcut icon" href="{{ asset('favicon(copy).png') }}"/>    
+    <link href="{{ asset('/css/bootstrap.min.css') }}" rel="stylesheet">
+    <script src="{{ asset('js/jquery-1.11.3.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
 </head>
 <body>
-	<nav class="navbar navbar-default">
-		<div class="container-fluid">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-					<span class="sr-only">Toggle Navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" href="{{ url('/') }}">Laravel</a>
-			</div>
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="{{ url('/') }}">E-learning</a>
+            </div>
 
-			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-				<ul class="nav navbar-nav navbar-right">
-					@if (Auth::guest())
-						<li><a href="{{ url('/auth/login') }}">Login</a></li>
-						<li><a href="{{ url('/auth/register') }}">Register</a></li>
-					@else
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
-							<ul class="dropdown-menu" role="menu">
-								<li><a href="{{ url('/auth/logout') }}">Logout</a></li>
-							</ul>
-						</li>
-					@endif
-				</ul>
-			</div>
-		</div>
-	</nav>
-	
-	<div class="container-fluid">
-		@yield('content')
-	</div>
-	
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                @if ($currentUser)
+                    <ul class="nav navbar-nav">
+                        <li><a href="{{ url('words') }}">Word List</a></li>
+                        <li><a href="#">Category</a></li>
+                        <li><a href="#">Lesson</a></li>
+                        <li><a href="#">Result</a></li>
+                        <li><a href="#">User Show</a></li>
+                    </ul>
+                @endif
+                <ul class="nav navbar-nav navbar-right">
+                    @if (!$currentUser)
+                        <li><a href="{{ url('/auth/login') }}">Login</a></li>
+                        <li><a href="{{ url('/auth/register') }}">Register</a></li>
+                    @else
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ $currentUser->name }} <span class="caret"></span></a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="{{ url('users/'.$currentUser->id) }}">Profile</a></li>
+                                <li><a href="#">Change password</a></li>
+                                @if ($currentUser->isAdmin())
+                                    <li role="separator" class="divider"></li>
+                                    <li><a href="{{ url('admin') }}">Admin Panel</a></li>
+                                @endif
+                                <li role="separator" class="divider"></li>
+                                <li><a href="{{ url('/auth/logout') }}">Logout</a></li>
+                            </ul>
+                        </li>
+                    @endif
+                </ul>
+            </div>
+        </div>
+    </nav>
+    
+    <div class="container-fluid">
+        @yield('content')
+    </div>
+    
 </body>
 </html>
